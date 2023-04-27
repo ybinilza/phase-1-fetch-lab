11f7207d75c29e930f683f9ba052f66ab9b821a6
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-04-27 01:44:20 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  /*fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+  */
+  return (fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(json => renderBooks(json)))
+
 }
 
 function renderBooks(books) {
